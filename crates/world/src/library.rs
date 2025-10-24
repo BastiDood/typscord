cffea146eacef2f90b6ec92ef2a51abac82a57a9
Basti Ortiz <ortiz@bastidood.dev>
2025-10-25 05:51:48 +0800

    fix(world): resolve breaking changes

--- crates/world/src/library.rs
+++ crates/world/src/library.rs
@@ -1,4 +1,5 @@
 use std::sync::LazyLock;
-use typst::{Library, utils::LazyHash};
+use typst::{Library, LibraryExt as _, utils::LazyHash};
 
-pub static LIBRARY: LazyLock<LazyHash<Library>> = LazyLock::new(Default::default);
+pub static LIBRARY: LazyLock<LazyHash<Library>> =
+	LazyLock::new(|| LazyHash::new(Library::default()));
